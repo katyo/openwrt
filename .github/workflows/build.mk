fc0654dce33aa0704250fc459985c044c5592bb8
K. <kayo@illumium.org>
2026-04-07 14:52:39 +0300

    Add zram-swap

--- .github/workflows/build.mk
+++ .github/workflows/build.mk
@@ -37,6 +37,7 @@ ramips_mt7621:xiaomi_redmi-router-ac2100=y
 endef
 
 define MODULES
+zram=y
 nf-nathelper=y
 nf-nathelper-extra=y
 nfnetlink=y
@@ -53,6 +54,7 @@ ip6tables=y
 endef
 
 define PACKAGES
+zram-swap=y
 iptables-mod-nfqueue=y
 iptables-mod-conntrack-extra=y
 wpad-basic-mbedtls=m
