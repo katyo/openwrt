5566f1bd87c947ab9f0557c52f856010d1130fef
K. <kayo@illumium.org>
2026-09-13 10:52:29 +0300

    Remove zapret and xray to save space

--- .github/workflows/build.mk
+++ .github/workflows/build.mk
@@ -76,15 +76,11 @@ luci-app-unbound=y
 luci-app-adblock-fast=y
 easytier=y
 luci-app-easytier=y
-luci-app-xray=y
-luci-app-xray-status=y
 gawk=y
 grep=y
 sed=y
 nano=y
 coreutils-sort=y
-zapret=y
-luci-app-zapret=y
 curl=y
 ca-certificates=y
 endef
